cd502a7482dcaefd355f23fc7877f735d16b0377
Haebaru Kana <kana.sakura1106@gmail.com>
2019-03-09 23:43:07 +0900

    Toggle the header menu with or without login

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => 'auth'], function(){
+    //ホーム画面
+    Route::get('/', 'HomeController@index')->name('home');
 
-//ホーム画面
-Route::get('/', 'HomeController@index')->name('home');
+    //認証画面
+    Auth::routes();
 
-//認証画面
-Auth::routes();
+    //フォルダ別タスク一覧
+    Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
-//フォルダ別タスク一覧
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+    // カテゴリ作成画面のルーティング
+    Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+    Route::post('/folders/create', 'FolderController@create');
 
-// カテゴリ作成画面のルーティング
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
+    // タスク作成画面のルーティング
+    Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
-// タスク作成画面のルーティング
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+    // タスク編集画面のルーティング
+    Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+});
 
-// タスク編集画面のルーティング
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Auth::rotes();
